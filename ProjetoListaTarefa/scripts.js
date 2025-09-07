e9c30c7ad1bc9d6e42c53f3a61bfcf4501994b03
DanielBerg <danielbergberg5@gmail.com>
2025-09-07 17:56:59 -0300

    Add files via upload

---
+++ ProjetoListaTarefa/scripts.js
@@ -0,0 +1,20 @@
+const { createElement } = require("react");
+
+function adicionarTarefa() {
+  let mensagem = "Tarefa adicionada com sucesso!";
+  document.getElementById("mensagem").textContent = mensagem;
+
+  let inputTarefa = document.getElementById("tarefa");
+
+  let tarefa = inputTarefa.value;
+
+  let listaTarefa= document.getElementById("listadetarefa");
+
+  let li= document.createElement("li");
+
+  li.textContent= tarefa
+
+  listaTarefa.appendChild(li);
+
+ 
+}
